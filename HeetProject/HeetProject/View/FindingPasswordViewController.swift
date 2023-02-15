004cf299e14df940f69acccda5f5523ccae55e7c
cathy171 <mooooongdu171@gmail.com>
2023-02-15 17:54:52 +0900

    feat: 인증 시 timer 구현

--- HeetProject/HeetProject/View/FindingPasswordViewController.swift
+++ HeetProject/HeetProject/View/FindingPasswordViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 import SnapKit
 
 class FindingPasswordViewController: UIViewController {
+  var timer: Timer?
+  var emailText: String?
+  var code: String?
+  var timeSet: Int = 180
   private let emailLabel = {
     let label = UILabel()
     label.text = "이메일"
@@ -26,12 +30,29 @@ class FindingPasswordViewController: UIViewController {
     let button = UIButton()
     button.setTitle("인증 요청", for: .normal)
     button.setTitleColor(ColorManager.BackgroundColor, for: .normal)
-    button.layer.cornerRadius = 25
+    button.layer.cornerRadius = 20
     button.layer.borderWidth = 1
     button.layer.borderColor = ColorManager.BackgroundColor?.cgColor
-    //    button.addTarget(self, action: #selector(didTapValidateNumber), for: .allTouchEvents)
+    button.addTarget(self, action: #selector(didTapValidateEmail), for: .touchDown)
     return button
   }()
+  @objc private func didTapValidateEmail() {
+    timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+      self.timeSet -= 1
+      var minute = self.timeSet / 60
+      var seconds = self.timeSet % 60
+      if self.timeSet > 0 {
+        self.timeLabel.text = String(format: "%02d", minute) + ":" + String(format: "%02d", seconds)
+      } else {
+        self.timeLabel.text = "인증 시간 만료"
+        timer.invalidate()
+      }
+    }
+    validateNumberButton.isUserInteractionEnabled = false
+    validateNumberButton.backgroundColor = .gray
+    requestButton.isHidden = false
+    requestLabel.isHidden = false
+  }
   private let validateNumber = {
     let textField = UITextField()
     textField.placeholder = "인증코드 입력"
@@ -43,7 +64,7 @@ class FindingPasswordViewController: UIViewController {
     let button = UIButton()
     button.setTitle("인증 요청", for: .normal)
     button.setTitleColor(ColorManager.BackgroundColor, for: .normal)
-    button.layer.cornerRadius = 25
+    button.layer.cornerRadius = 20
     button.layer.borderWidth = 1
     button.layer.borderColor = ColorManager.BackgroundColor?.cgColor
     //    button.addTarget(self, action: #selector(didTapValidateNumber), for: .allTouchEvents)
@@ -58,18 +79,58 @@ class FindingPasswordViewController: UIViewController {
     let label = UILabel()
     label.text = "인증번호를 받지 못하셨나요?"
     label.textColor = .gray
+    label.isHidden = true
     return label
   }()
   private let requestButton = {
     let button = UIButton()
     button.setTitle("재전송하기", for: .normal)
     button.setTitleColor(UIColor.gray, for: .normal)
+    button.isHidden = true
+    button.addTarget(self, action: #selector(didTapValidate), for: .touchDown)
     return button
   }()
+  @objc private func didTapReValidate() {
+    timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+      self.timeSet -= 1
+      var minute = self.timeSet / 60
+      var seconds = self.timeSet % 60
+      if self.timeSet > 0 {
+        self.timeLabel.text = String(format: "%02d", minute) + ":" + String(format: "%02d", seconds)
+      } else {
+        self.timeLabel.text = "인증 시간 만료"
+        timer.invalidate()
+      }
+    }
+  }
+  @objc private func didTapValidate() {
+    if ((timer?.isValid) != nil) {
+      timer?.invalidate()
+      timeSet = 180
+    }
+    timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+      self.timeSet -= 1
+      var minute = self.timeSet / 60
+      var seconds = self.timeSet % 60
+      if self.timeSet > 0 {
+        self.timeLabel.text = String(format: "%02d", minute) + ":" + String(format: "%02d", seconds)
+      } else {
+        self.timeLabel.text = "인증 시간 만료"
+        timer.invalidate()
+      }
+    }
+  }
   private let lineView2 = {
     let view = UIView()
     view.backgroundColor = ColorManager.BackgroundColor
     return view
+  }()
+  private let timeLabel = {
+    let label = UILabel()
+    label.text = "03:00"
+    label.font = .systemFont(ofSize: 15, weight: .bold)
+    label.textColor = ColorManager.BackgroundColor
+    return label
   }()
   private let nextButton = {
     let button = UIButton()
@@ -78,7 +139,7 @@ class FindingPasswordViewController: UIViewController {
     button.setImage(UIImage(systemName: "chevron.right"), for: .normal)
     button.tintColor = ColorManager.BackgroundColor
     button.semanticContentAttribute = .forceRightToLeft
-    //    button.addTarget(self, action: #selector(didTapNext), for: .touchDown)
+    button.addTarget(FindingPasswordViewController.self, action: #selector(didTapNext), for: .touchDown)
     return button
   }()
   override func viewDidLoad() {
@@ -86,10 +147,16 @@ class FindingPasswordViewController: UIViewController {
     view.backgroundColor = .white
     self.navigationItem.title = "비밀번호 찾기"
     self.navigationController?.navigationBar.topItem?.backButtonTitle = ""
+    emailInput.delegate = self
+    validateNumber.delegate = self
     configureUI()
   }
+  @objc private func didTapNext() {
+    let vc = FindingPasswordDetailViewController()
+    self.navigationController?.pushViewController(vc, animated: true)
+  }
   private func configureUI() {
-    [emailInput, emailLabel, lineView, lineView2, validateNumber, validateNumberButton, validateNumberButton2, requestLabel, requestButton, nextButton]
+    [emailInput, emailLabel, lineView, lineView2, validateNumber, validateNumberButton, validateNumberButton2, requestLabel, requestButton, nextButton, timeLabel]
       .forEach {
         view.addSubview($0)
       }
@@ -114,8 +181,12 @@ class FindingPasswordViewController: UIViewController {
       $0.height.equalTo(40)
     }
     validateNumber.snp.makeConstraints {
-      $0.top.equalTo(lineView.snp.bottom).offset(30)
+      $0.top.equalTo(lineView.snp.bottom).offset(50)
       $0.leading.equalToSuperview().offset(20)
+    }
+    timeLabel.snp.makeConstraints {
+      $0.trailing.equalTo(validateNumberButton2.snp.leading).offset(20)
+      $0.centerY.equalTo(validateNumber.snp.centerY)
     }
     lineView2.snp.makeConstraints {
       $0.top.equalTo(validateNumber.snp.bottom).offset(5)
@@ -130,7 +201,7 @@ class FindingPasswordViewController: UIViewController {
       $0.height.equalTo(40)
     }
     requestLabel.snp.makeConstraints {
-      $0.top.equalTo(lineView2.snp.bottom).offset(10)
+      $0.top.equalTo(lineView2.snp.bottom).offset(20)
       $0.leading.equalToSuperview().offset(20)
     }
     requestButton.snp.makeConstraints {
@@ -141,5 +212,11 @@ class FindingPasswordViewController: UIViewController {
       $0.bottom.equalToSuperview().offset(-300)
       $0.trailing.equalToSuperview().offset(-20)
     }
+  }
+}
+extension FindingPasswordViewController: UITextFieldDelegate {
+  func textFieldDidChangeSelection(_ textField: UITextField) {
+    emailText = emailInput.text
+    code = validateNumber.text
   }
 }
--- HeetProject/HeetProject/View/SignupViewController.swift
+++ HeetProject/HeetProject/View/SignupViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 import SnapKit
 
 class SignupViewController: UIViewController {
+  var emailText: String?
+  var timeSet: Int = 180
+  var timer: Timer?
   private let emailTextField = {
     let textField = UITextField()
     textField.placeholder = "이메일 입력"
@@ -25,7 +28,7 @@ class SignupViewController: UIViewController {
     button.setTitle("이메일 인증하기", for: .normal)
     button.setTitleColor(UIColor.white, for: .normal)
     button.layer.cornerRadius = 25
-    button.addTarget(self, action: #selector(didTapValidate), for: .allTouchEvents)
+    button.addTarget(self, action: #selector(didTapValidate), for: .touchDown)
     return button
   }()
   private let emailImage = {
@@ -37,7 +40,14 @@ class SignupViewController: UIViewController {
   }()
   private let emailLabel = {
     let label = UILabel()
-    label.text = "이메일 전송이 성공되었습니다.\n인증 코드 6자리를 입력해주세요."
+    label.text = "이메일 전송이 성공되었습니다."
+    label.textColor = .gray
+    label.isHidden = true
+    return label
+  }()
+  private let emailLabel2 = {
+    let label = UILabel()
+    label.text = "인증 코드 6자리를 입력해주세요."
     label.textColor = .gray
     label.isHidden = true
     return label
@@ -49,6 +59,13 @@ class SignupViewController: UIViewController {
     textField.leftViewMode = .always
     return textField
   }()
+  private let timeLabel = {
+    let label = UILabel()
+    label.text = "03:00"
+    label.font = .systemFont(ofSize: 15, weight: .bold)
+    label.textColor = ColorManager.BackgroundColor
+    return label
+  }()
   private let validateNumberButton = {
     let button = UIButton()
     button.setTitle("인증 요청", for: .normal)
@@ -56,7 +73,7 @@ class SignupViewController: UIViewController {
     button.layer.cornerRadius = 25
     button.layer.borderWidth = 1
     button.layer.borderColor = ColorManager.BackgroundColor?.cgColor
-    button.addTarget(self, action: #selector(didTapValidateNumber), for: .allTouchEvents)
+    button.addTarget(SignupViewController.self, action: #selector(didTapValidateNumber), for: .touchDown)
     return button
   }()
   private let lineView = {
@@ -68,12 +85,15 @@ class SignupViewController: UIViewController {
     let label = UILabel()
     label.text = "인증번호를 받지 못하셨나요?"
     label.textColor = .gray
+    label.isHidden = true
     return label
   }()
   private let requestButton = {
     let button = UIButton()
     button.setTitle("재전송하기", for: .normal)
     button.setTitleColor(UIColor.gray, for: .normal)
+    button.isHidden = true
+    button.addTarget(self, action: #selector(didTapReValidate), for: .touchDown)
     return button
   }()
   private let nextButton = {
@@ -83,17 +103,20 @@ class SignupViewController: UIViewController {
     button.setImage(UIImage(systemName: "chevron.right"), for: .normal)
     button.tintColor = ColorManager.BackgroundColor
     button.semanticContentAttribute = .forceRightToLeft
-    button.addTarget(self, action: #selector(didTapNext), for: .allTouchEvents)
+    button.addTarget(self, action: #selector(didTapNext), for: .touchDown)
     return button
   }()
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .white
+    emailTextField.delegate = self
     configureUI()
-    self.navigationController?.title = "회원 가입"
+    self.navigationItem.title = "회원 가입"
+    self.navigationController?.navigationBar.topItem?.backButtonTitle = ""
+    self.navigationController?.navigationBar.topItem?.backBarButtonItem?.tintColor = .black
   }
   private func configureUI() {
-    [emailTextField, validateButton, emailImage, emailLabel, validateNumber, validateNumberButton, lineView, requestButton, requestLabel, nextButton]
+    [emailTextField, validateButton, emailImage, emailLabel, validateNumber, validateNumberButton, lineView, requestButton, requestLabel, nextButton, emailLabel2, timeLabel]
       .forEach { view.addSubview($0) }
     emailTextField.snp.makeConstraints {
       $0.centerX.equalToSuperview()
@@ -117,43 +140,87 @@ class SignupViewController: UIViewController {
       $0.left.equalTo(emailImage.snp.right).offset(20)
       $0.top.equalTo(emailImage.snp.top)
     }
+    emailLabel2.snp.makeConstraints {
+      $0.left.equalTo(emailImage.snp.right).offset(20)
+      $0.top.equalTo(emailLabel.snp.bottom).offset(2)
+    }
     validateNumber.snp.makeConstraints {
       $0.left.equalTo(emailImage.snp.left)
       $0.top.equalTo(emailImage.snp.bottom).offset(50)
     }
     lineView.snp.makeConstraints {
-      $0.top.equalTo(validateNumber.snp.bottom).offset(30)
+      $0.top.equalTo(validateNumber.snp.bottom).offset(10)
       $0.left.equalTo(validateButton.snp.left)
       $0.width.equalTo(350)
       $0.height.equalTo(1)
     }
     validateNumberButton.snp.makeConstraints {
-      $0.top.equalTo(validateNumber.snp.top)
+      $0.bottom.equalTo(lineView.snp.top).offset(-10)
       $0.right.equalTo(lineView.snp.right)
       $0.width.equalTo(120)
       $0.height.equalTo(50)
     }
     requestLabel.snp.makeConstraints {
-      $0.top.equalTo(lineView.snp.bottom).offset(40)
+      $0.top.equalTo(lineView.snp.bottom).offset(20)
       $0.left.equalTo(lineView.snp.left).offset(20)
     }
     requestButton.snp.makeConstraints {
-      $0.right.equalTo(lineView.snp.right)
-      $0.top.equalTo(requestLabel.snp.top)
+      $0.right.equalTo(lineView.snp.right).offset(-10)
+      $0.centerY.equalTo(requestLabel.snp.centerY)
     }
     nextButton.snp.makeConstraints {
       $0.top.equalTo(requestButton.snp.bottom).offset(100)
       $0.right.equalTo(lineView.snp.right)
     }
+    timeLabel.snp.makeConstraints {
+      $0.trailing.equalTo(requestButton.snp.leading).offset(10)
+      $0.centerY.equalTo(validateNumber.snp.centerY)
+    }
   }
   @objc private func didTapValidate() {
     emailLabel.isHidden = false
     emailImage.isHidden = false
+    validateButton.isUserInteractionEnabled = false
+    validateButton.backgroundColor = .gray
+    timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+      self.timeSet -= 1
+      var minute = self.timeSet / 60
+      var seconds = self.timeSet % 60
+      if self.timeSet > 0 {
+        self.timeLabel.text = String(format: "%02d", minute) + ":" + String(format: "%02d", seconds)
+      } else {
+        self.timeLabel.text = "인증 시간 만료"
+        timer.invalidate()
+      }
+    }
+    requestLabel.isHidden = false
+    requestButton.isHidden = false
   }
   @objc private func didTapValidateNumber() {
-    
+  }
+  @objc private func didTapReValidate() {
+    timer?.invalidate()
+    timeSet = 180
+    timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+      self.timeSet -= 1
+      var minute = self.timeSet / 60
+      var seconds = self.timeSet % 60
+      if self.timeSet > 0 {
+        self.timeLabel.text = String(format: "%02d", minute) + ":" + String(format: "%02d", seconds)
+      } else {
+        self.timeLabel.text = "인증 시간 만료"
+        timer.invalidate()
+      }
+    }
   }
   @objc private func didTapNext() {
-    self.navigationController?.pushViewController(SignupDetailViewController(), animated: false)
+    let vc = SignupDetailViewController()
+    vc.emailtextLabel.text = "emailtiettst"
+    self.navigationController?.pushViewController(vc, animated: true)
+  }
+}
+extension SignupViewController: UITextFieldDelegate {
+  func textFieldDidChangeSelection(_ textField: UITextField) {
+    emailText = emailTextField.text
   }
 }

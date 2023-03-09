04e0151de8f53c71cd956d92f3fb2e3c70d02aca
cathy171 <mooooongdu171@gmail.com>
2023-03-09 16:28:09 +0900

    feat: custom tapbar controller 구현

--- HeetProject/HeetProject/View/Signup/CustomTabBarController.swift
+++ HeetProject/HeetProject/View/Signup/CustomTabBarController.swift
@@ -22,31 +22,37 @@ class CustomTabBarController: UITabBarController {
   let naviView: UIView = {
     let view = UIView()
     view.backgroundColor = ColorManager.BackgroundColor
-    view.layer.cornerRadius = 10
+    view.layer.cornerRadius = 15
     return view
   }()
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    self.tabBarController?.tabBar.backgroundColor = .systemGray5
+  }
   override func viewDidLoad() {
+    self.navigationController?.navigationBar.isHidden = true
     view.backgroundColor = .white
     super.viewDidLoad()
+    self.tabBarController?.tabBar.backgroundColor = .systemGray5
     self.tabBarItem.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.init(red: 255, green: 120, blue: 120, alpha: 1)], for: .selected)
     self.tabBar.tintColor = ColorManager.BackgroundColor
     let firstVC = UINavigationController(rootViewController: MainViewController())
     firstVC.navigationBar.topItem?.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "heart"), style: .done, target: self, action: #selector(didTap))
     let search = UIBarButtonItem(image: UIImage(named: "search"), style: .done, target: self, action: #selector(didTapSearch))
     let scrap = UIBarButtonItem(image: UIImage(named: "scrap"), style: .done, target: self, action: #selector(didTapScrap))
-    firstVC.navigationBar.topItem?.rightBarButtonItems = [search, scrap]
+    firstVC.navigationBar.topItem?.rightBarButtonItems = [scrap, search]
     firstVC.navigationBar.topItem?.leftBarButtonItem = UIBarButtonItem(customView: naviView)
-    //    var customTabBarItem:UITabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "heart"), selectedImage: UIImage(named: "YOUR_IMAGE_NAME"))
-    //    firstVC.tabBarItem = customTabBarItem
     firstVC.tabBarItem.image = UIImage(named: "Location")
     firstVC.tabBarItem.title = "동네 정보"
     firstVC.navigationBar.tintColor = .black
-    let secondVC = FollowingViewController()
+    let secondVC = UINavigationController(rootViewController: FollowingViewController())
     secondVC.tabBarItem.title = "팔로잉"
     secondVC.tabBarItem.image = UIImage(named: "followTab")
-    let thirdVC = MyPageViewController()
+    let thirdVC = UINavigationController(rootViewController: MyPageViewController())
     thirdVC.tabBarItem.title = "마이페이지"
     thirdVC.tabBarItem.image = UIImage(named: "mypageTab")
+    thirdVC.navigationBar.topItem?.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "LoginLogo"), style: .done, target: self, action: nil)
+    thirdVC.navigationBar.topItem?.leftBarButtonItem?.tintColor = ColorManager.BackgroundColor
     self.setViewControllers([firstVC, secondVC, thirdVC], animated: true)
     naviView.addSubview(locationImage)
     naviView.addSubview(locationLabel)
@@ -57,7 +63,7 @@ class CustomTabBarController: UITabBarController {
     locationImage.snp.makeConstraints {
       $0.left.equalTo(naviView.snp.left).offset(6)
       $0.centerY.equalTo(naviView.snp.centerY)
-      $0.width.equalTo(14)
+      $0.width.equalTo(17)
       $0.height.equalTo(17)
     }
     locationLabel.snp.makeConstraints {
@@ -66,12 +72,9 @@ class CustomTabBarController: UITabBarController {
     }
   }
   @objc private func didTap() {
-    
   }
   @objc private func didTapSearch() {
-    
   }
   @objc private func didTapScrap() {
-    
   }
 }
